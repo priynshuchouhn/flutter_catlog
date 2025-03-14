7224cb544b97b845a42f89ddd2c4a3be0a0fe8ff
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-14 20:31:42 +0530

    Chapter 13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,11 +43,41 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(16.0),
         child:
             CatlogModel.items.isNotEmpty
-                ? ListView.builder(
+                ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
                   itemCount: CatlogModel.items.length,
-                  itemBuilder:
-                      (context, index) =>
-                          ItemWidget(item: CatlogModel.items[index]),
+                  itemBuilder: (context, index) {
+                    final item = CatlogModel.items[index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: GridTile(
+                        header: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(color: Colors.deepPurple),
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        footer: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(color: Colors.black),
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        child: Image.network(item.image),
+                      ),
+                    );
+                  },
                 )
                 : Center(child: CircularProgressIndicator()),
       ),
